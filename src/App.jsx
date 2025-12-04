962935336d11d5a0df232dfab577d5bba5346a7c
viktoriadochevska <viktoria.dochevska@gmail.com>
2025-12-04 13:51:19 +0100

    Integrate Firebase for participant data management

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,67 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { ref, onValue, set } from "firebase/database";
+import { database } from "./firebase";
 import FramingStudyApp from "./FramingStudyApp.jsx";
 import SurveyPage from "./SurveyPage.jsx";
 
 export default function App() {
   const [participants, setParticipants] = useState([]);
 
+  // Load participants from Firebase
+  useEffect(() => {
+    const participantsRef = ref(database, 'participants');
+    const unsubscribe = onValue(participantsRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        setParticipants(Object.values(data));
+      } else {
+        setParticipants([]);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  // Save participants to Firebase
+  function updateParticipants(newParticipants) {
+    const participantsRef = ref(database, 'participants');
+    const participantsObj = {};
+    newParticipants.forEach(p => {
+      participantsObj[p.id] = p;
+    });
+    set(participantsRef, participantsObj);
+  }
+
   function submitRatings(id, ratings) {
-    setParticipants((s) => s.map((p) => (p.id === id ? { ...p, ratings } : p)));
+    const updated = participants.map((p) => 
+      p.id === id ? { ...p, ratings } : p
+    );
+    updateParticipants(updated);
   }
 
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<FramingStudyApp participants={participants} setParticipants={setParticipants} />}
+        <Route 
+          path="/" 
+          element={
+            <FramingStudyApp 
+              participants={participants} 
+              setParticipants={updateParticipants} 
+            />
+          } 
         />
-        <Route
-          path="/survey/:participantId"
-          element={<SurveyPage participants={participants} submitRatings={submitRatings} />}
+        <Route 
+          path="/survey/:participantId" 
+          element={
+            <SurveyPage 
+              participants={participants} 
+              submitRatings={submitRatings} 
+            />
+          } 
         />
       </Routes>
     </Router>
   );
 }
-
-
